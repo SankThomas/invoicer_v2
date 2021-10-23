3becc7db01768272b4ad3f335398c5c90f10d203
Thomas Sankara <thomassankara74@gmail.com>
2021-10-23 08:40:28 +0300

    Dates component

---
+++ src/components/Dates.js
@@ -0,0 +1,19 @@
+export default function Dates({ invoiceNumber, invoiceDate, dueDate }) {
+  return (
+    <>
+      <article className="my-5 flex items-end justify-end">
+        <ul>
+          <li className="p-1 ">
+            <span className="font-bold">Invoicer number:</span> {invoiceNumber}
+          </li>
+          <li className="p-1 bg-gray-100">
+            <span className="font-bold">Invoice date:</span> {invoiceDate}
+          </li>
+          <li className="p-1 ">
+            <span className="font-bold">Due date:</span> {dueDate}
+          </li>
+        </ul>
+      </article>
+    </>
+  )
+}
