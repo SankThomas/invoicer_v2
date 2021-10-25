1a2cb71d068d2e1d2bd99797009c60fd5b0cef78
Thomas Sankara <thomassankara74@gmail.com>
2021-10-25 09:39:03 +0300

    MainDetails - your details - component

---
+++ src/components/MainDetails.js
@@ -0,0 +1,10 @@
+export default function MainDetails({ name, address }) {
+  return (
+    <>
+      <section className="flex flex-col items-end justify-end">
+        <h2 className="font-bold text-xl uppercase md:text-4xl">{name}</h2>
+        <p>{address}</p>
+      </section>
+    </>
+  )
+}
