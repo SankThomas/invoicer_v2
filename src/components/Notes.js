eb26dca2e6f88abcb206c8a2779d1b73d5075f74
Thomas Sankara <thomassankara74@gmail.com>
2021-10-25 09:39:17 +0300

    Additional notes component

---
+++ src/components/Notes.js
@@ -0,0 +1,9 @@
+export default function Notes({ notes }) {
+  return (
+    <>
+      <section className="mb-5">
+        <p className="lg:w-1/2 text-justify">{notes}</p>
+      </section>
+    </>
+  )
+}
