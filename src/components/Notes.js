48a28b8716c2b277554966e0500fe248bd5df42d
Thomas Sankara <thomassankara74@gmail.com>
2021-10-28 11:13:59 +0300

    increase margin

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,7 @@
 export default function Notes({ notes }) {
   return (
     <>
-      <section className="mb-5">
+      <section className="mt-10 mb-5">
         <p className="lg:w-1/2 text-justify">{notes}</p>
       </section>
     </>
