4ddf826bc975e0a2190ddaed65964247ce7854ce
Thomas Sankara <thomassankara74@gmail.com>
2021-10-28 11:14:10 +0300

    update Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,30 @@
-export default function Table() {
+import React from "react"
+
+export default function Table({ list }) {
   return (
     <>
-      <div className="my-5">this is the table</div>
+      <table width="100%" className="mb-10">
+        <thead>
+          <tr className="bg-gray-100 p-1">
+            <td className="font-bold">Description</td>
+            <td className="font-bold">Quantity</td>
+            <td className="font-bold">Price</td>
+            <td className="font-bold">Amount</td>
+          </tr>
+        </thead>
+        {list.map(({ id, description, quantity, price, amount }) => (
+          <React.Fragment key={id}>
+            <tbody>
+              <tr>
+                <td>{description}</td>
+                <td>{quantity}</td>
+                <td>{price}</td>
+                <td>{amount}</td>
+              </tr>
+            </tbody>
+          </React.Fragment>
+        ))}
+      </table>
     </>
   )
 }
