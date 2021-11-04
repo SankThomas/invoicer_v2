87c8258c12ccb27e5461b1efc5ff27f90571e546
Thomas Sankara <thomassankara74@gmail.com>
2021-11-04 08:15:50 +0300

    increase margin

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,7 +1,7 @@
 export default function Dates({ invoiceNumber, invoiceDate, dueDate }) {
   return (
     <>
-      <article className="my-5 flex items-end justify-end">
+      <article className="mt-10 mb-14 flex items-end justify-end">
         <ul>
           <li className="p-1 ">
             <span className="font-bold">Invoicer number:</span> {invoiceNumber}
