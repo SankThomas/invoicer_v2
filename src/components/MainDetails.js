c201a7489c38e4db2fcecca200722a4a770d9900
Thomas Sankara <thomassankara74@gmail.com>
2021-11-04 08:16:24 +0300

    increase margin on h2

--- src/components/MainDetails.js
+++ src/components/MainDetails.js
@@ -2,7 +2,7 @@ export default function MainDetails({ name, address }) {
   return (
     <>
       <section className="flex flex-col items-end justify-end">
-        <h2 className="font-bold text-xl uppercase md:text-4xl">{name}</h2>
+        <h2 className="font-bold text-xl uppercase mb-1 md:text-4xl">{name}</h2>
         <p>{address}</p>
       </section>
     </>
