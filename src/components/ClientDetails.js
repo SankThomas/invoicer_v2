1282e929167742ee85a610877d4df5a6db5dd9f9
Thomas Sankara <thomassankara74@gmail.com>
2021-11-04 08:15:33 +0300

    style h2

--- src/components/ClientDetails.js
+++ src/components/ClientDetails.js
@@ -1,8 +1,8 @@
 export default function ClientDetails({ clientName, clientAddress }) {
   return (
     <>
-      <section className="mt-5">
-        <h2 className="text-xl uppercase">{clientName}</h2>
+      <section className="mt-10">
+        <h2 className="text-2xl uppercase font-bold mb-1">{clientName}</h2>
         <p>{clientAddress}</p>
       </section>
     </>
