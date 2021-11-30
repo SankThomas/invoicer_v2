074bdf746e1054deb21f52377ed23f217b598651
Thomas Sankara <thomassankara74@gmail.com>
2021-11-30 19:37:39 +0300

    add context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import { AuthContextProvider } from "./context/auth"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 )
