0cb735560567e39a5ce0cc4a5b522551eddc10c6
Thomas Sankara <thomassankara74@gmail.com>
2021-12-01 17:58:25 +0300

    create account button

---
+++ src/buttons/CreateAccount.js
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+import AuthContext from "../context/auth"
+
+export default function CreateAccount() {
+  const { login } = useContext(AuthContext)
+
+  return (
+    <>
+      <button
+        onClick={login}
+        className="bg-blue-500 py-2 px-6 rounded font-bold hover:bg-transparent border-2 border-blue-500 text-white transition-all duration-500 hover:opacity-75 hover:bg-blue-800 hover:border-blue-800 shadow-lg"
+      >
+        Create Account
+      </button>
+    </>
+  )
+}
