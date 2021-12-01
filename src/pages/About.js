8a6f2f0c381d5a93bff292554c9b67a420aca241
Thomas Sankara <thomassankara74@gmail.com>
2021-12-01 17:59:51 +0300

    About page

---
+++ src/pages/About.js
@@ -0,0 +1,40 @@
+import { Footer } from "./index"
+import { LogIn, CreateAccount } from "../buttons"
+
+export default function About() {
+  return (
+    <>
+      <section className="about py-10 md:py-0">
+        <div className="px-5 2xl:px-0 xl:max-w-7xl xl:mx-auto">
+          <div className="overlay">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center mb-3 text-white lg:mb-8">
+              About Invoicer
+            </h1>
+            <p className="text-white text-center text-xl lg:w-2/3 lg:mx-auto">
+              Invoicer is an Invoicing application designed and built by Thomas
+              Sankara, whose main purpose is to - as the name suggests - create
+              and send invoices to clients, because this is something I've
+              struggled with before. I hope it helps you as well.
+            </p>
+            <p className="text-white text-center text-xl lg:w-2/3 lg:mx-auto mt-5">
+              Please note that even though most of the functionality is done,
+              there are still some things to add such as saving invoices, and
+              improving the Login and Logout experience. Thank you as you bear
+              with me.
+            </p>
+
+            <ul className="md:flex items-center justify-between mt-10 xl:max-w-3xl xl:mx-auto">
+              <li className="flex flex-col text-white mb-5 md:flex-row md:items-center md:mb-0 md:mr-5 lg:mr-20">
+                Don't have an account? &nbsp;&nbsp; <CreateAccount />
+              </li>
+              <li className="flex flex-col text-white md:flex-row md:items-center">
+                Already have an account? &nbsp;&nbsp; <LogIn />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
+  )
+}
