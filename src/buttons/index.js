e4abeb960c9f142abd2023c99456aa5efe1a0e59
Thomas Sankara <thomassankara74@gmail.com>
2021-12-02 00:38:58 +0300

    add Logout button

--- src/buttons/index.js
+++ src/buttons/index.js
@@ -1,4 +1,5 @@
 import CreateAccount from "../buttons/CreateAccount"
 import LogIn from "../buttons/LogIn"
+import LogOut from "../buttons/LogOut"
 
-export { CreateAccount, LogIn }
+export { CreateAccount, LogIn, LogOut }
