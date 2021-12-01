cc69ffc9b6f90ddf9c24efddd46c2ed55825f41a
Thomas Sankara <thomassankara74@gmail.com>
2021-12-01 18:01:05 +0300

    Header component

---
+++ src/pages/components/Header.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { navbar } from "../../data/data"
+import { FaBars } from "react-icons/fa"
+import { CreateAccount } from "../../buttons"
+
+export default function Header() {
+  const [links] = useState(navbar)
+
+  const handleClick = () => {
+    const navbar = document.querySelector(".navbar")
+    const listItems = document.querySelectorAll(".list-item")
+
+    navbar.classList.toggle("open")
+
+    listItems.forEach((item) =>
+      item.addEventListener("click", () => navbar.classList.remove("open"))
+    )
+  }
+
+  return (
+    <>
+      <header className="p-5 flex items-center justify-between xl:max-w-7xl lg:mx-auto 2xl:px-0">
+        <div>
+          <h2 className="font-bold text-4xl text-gray-800">
+            <Link to="/">Invoicer</Link>
+          </h2>
+        </div>
+
+        <nav className="navbar">
+          <ul>
+            {links.map(({ id, title, url }) => (
+              <React.Fragment key={id}>
+                <li key={id} className="list-item">
+                  <Link to={url}>{title}</Link>
+                </li>
+              </React.Fragment>
+            ))}
+
+            <li>
+              <CreateAccount />
+            </li>
+          </ul>
+        </nav>
+
+        <div className="lg:hidden">
+          <button
+            onClick={handleClick}
+            className="bg-blue-500 p-2 rounded hover:bg-blue-800 transition-all duration-500 hover:opacity-75"
+          >
+            <FaBars className="text-2xl text-white" />
+          </button>
+        </div>
+      </header>
+    </>
+  )
+}
