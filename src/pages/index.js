62b6a0db0653e940123eafe737ec8ccf75ebce53
Thomas Sankara <thomassankara74@gmail.com>
2021-12-01 18:00:05 +0300

    entry file

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Home from "./Home"
+import About from "./About"
+import SuggestionBox from "./SuggestionBox"
+import Header from "./components/Header"
+import Video from "./components/Video"
+import Testimonials from "./components/Testimonials"
+import CallToAction from "./components/CallToAction"
+import ScrollToTop from "./components/ScrollToTop"
+import ContactForm from "./components/ContactForm"
+import Footer from "./components/Footer"
+
+export {
+  Home,
+  About,
+  SuggestionBox,
+  Header,
+  Video,
+  Testimonials,
+  CallToAction,
+  ScrollToTop,
+  ContactForm,
+  Footer,
+}
