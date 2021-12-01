17bc5405d6f6fed19746329b2eb96370078d8cf8
Thomas Sankara <thomassankara74@gmail.com>
2021-12-02 00:38:47 +0300

    Logout button

---
+++ src/buttons/LogOut.js
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+import AuthContext from "../context/auth"
+
+export default function LogOut() {
+  const { logout } = useContext(AuthContext)
+
+  return (
+    <>
+      <button
+        onClick={logout}
+        className="bg-white py-2 px-6 rounded font-bold hover:bg-transparent border-2 border-white text-gray-800 transition-all duration-500 hover:opacity-75 shadow-lg hover:border-gray-800"
+      >
+        Log Out
+      </button>
+    </>
+  )
+}
