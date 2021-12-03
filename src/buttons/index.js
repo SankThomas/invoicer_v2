dba0d976dd836b5c9461b5b3493d01c46c2aefaf
Thomas Sankara <thomassankara74@gmail.com>
2021-12-03 19:05:42 +0300

    add donation button

--- src/buttons/index.js
+++ src/buttons/index.js
@@ -1,5 +1,6 @@
 import CreateAccount from "../buttons/CreateAccount"
 import LogIn from "../buttons/LogIn"
 import LogOut from "../buttons/LogOut"
+import DonateButton from "./DonateButton"
 
-export { CreateAccount, LogIn, LogOut }
+export { CreateAccount, LogIn, LogOut, DonateButton }
