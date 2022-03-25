4869a8a22d431659cd071ae5cfcd1c24e7ececd5
Thomas Sankara <thomassankara74@gmail.com>
2022-03-25 07:43:59 +0300

    add patreon button

--- src/buttons/DonateButton.js
+++ src/buttons/DonateButton.js
@@ -1,12 +1,17 @@
 export default function DonateButton() {
   return (
     <>
-      <form action="https://www.paypal.com/donate" method="post" target="_top">
+      {/* <form action="https://www.paypal.com/donate" method="post" target="_top">
         <input type="hidden" name="hosted_button_id" value="UCXYN5VX4FMPE" />
         <button className="bg-red-500 py-2 px-6 rounded font-bold hover:bg-transparent border-2 border-red-500 text-white transition-all duration-500 hover:opacity-75 hover:bg-red-500 hover:border-red-500 shadow-lg">
           Donate
         </button>
-      </form>
+      </form> */}
+      <a href="https://patreon.com/tsbsankara" target="_blank" rel="noreferrer">
+        <button className="bg-red-500 py-2 px-6 rounded font-bold hover:bg-transparent border-2 border-red-500 text-white transition-all duration-500 hover:opacity-75 hover:bg-red-500 hover:border-red-500 shadow-lg text-sm">
+          Patreon
+        </button>
+      </a>
     </>
   )
 }
