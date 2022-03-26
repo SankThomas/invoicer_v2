f96d3616ec1e5ebb001434d3bf02b49084767459
Thomas Sankara <thomassankara74@gmail.com>
2022-03-26 10:39:37 +0300

    update component

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -2,31 +2,34 @@ import React, { useState, useContext } from "react"
 import { Link } from "react-router-dom"
 import AuthContext from "../../context/auth"
 import { navbar } from "../../data/data"
-import { FaBars } from "react-icons/fa"
 import { CreateAccount, DonateButton, LogOut } from "../../buttons"
 
 export default function Header() {
   const [links] = useState(navbar)
   const { user } = useContext(AuthContext)
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleClick = () => {
+    setIsOpen(true)
     const navbar = document.querySelector(".navbar")
     const listItems = document.querySelectorAll(".list-item")
 
     navbar.classList.toggle("open")
+    setIsOpen(!isOpen)
 
-    listItems.forEach((item) =>
+    listItems.forEach((item) => {
       item.addEventListener("click", () => navbar.classList.remove("open"))
-    )
+      setIsOpen(false)
+    })
   }
 
   return (
     <>
       <header className="p-5 flex items-center justify-between xl:max-w-7xl lg:mx-auto 2xl:px-0">
         <div>
-          <h2 className="font-bold text-4xl text-gray-800">
+          {/* <h2 className="font-bold text-4xl text-gray-800">
             <Link to="/">Invoicer</Link>
-          </h2>
+          </h2> */}
         </div>
 
         <nav className="navbar">
@@ -34,7 +37,7 @@ export default function Header() {
             {links.map(({ id, title, url }) => (
               <React.Fragment key={id}>
                 <li key={id} className="list-item">
-                  <Link to={url} className="text-base">
+                  <Link to={url} className="text-base text-slate-700">
                     {title}
                   </Link>
                 </li>
@@ -51,9 +54,9 @@ export default function Header() {
         <div className="lg:hidden">
           <button
             onClick={handleClick}
-            className="bg-blue-500 p-2 rounded hover:bg-blue-800 transition-all duration-500 hover:opacity-75"
+            className="text-sm uppercase transition-all duration-500 text-slate-700"
           >
-            <FaBars className="text-2xl text-white" />
+            {isOpen ? "Close" : "Menu"}
           </button>
         </div>
       </header>
