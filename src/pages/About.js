d5be145e1519f00753dbdb73c0604d37f222dac2
Thomas Sankara <thomassankara74@gmail.com>
2022-03-26 10:39:14 +0300

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,56 +4,60 @@ import { LogIn, CreateAccount } from "../buttons"
 export default function About() {
   return (
     <>
-      <section className="about py-10 lg:py-20">
-        <div className="px-5 2xl:px-0 xl:max-w-7xl xl:mx-auto">
+      <section className="bg-teal-200 py-10 lg:py-32 ">
+        <div className="max-width">
           <div className="overlay">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center mb-3 text-white lg:mb-8">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center mb-3 text-slate-800 lg:mb-8">
               About Invoicer
             </h1>
-            <p className="text-white text-center text-xl lg:w-2/3 lg:mx-auto">
+            <p className="text-slate-700 text-center text-xl lg:w-2/3 lg:mx-auto">
               Invoicer is an Invoicing application designed and built by Thomas
               Sankara, whose main purpose is to - as the name suggests - create
               and send invoices to clients, because this is something I've
               struggled with before. I hope it helps you as well.
             </p>
-            <p className="text-white text-center text-xl lg:w-2/3 lg:mx-auto mt-5">
+            <p className="text-slate-700 text-center text-xl lg:w-2/3 lg:mx-auto mt-5">
               Please note that even though most of the functionality is done,
               there are still some things to add such as saving invoices, and
               improving the Login and Logout experience. Thank you as you bear
               with me.
             </p>
-            <h3 className="text-2xl text-white mt-10 mb-5">
-              Some of the known issues include:
-            </h3>
-            <ol className="list-decimal text-white">
-              <li>
-                Users needing to re-log in every time they open the application.
-                - FIXED!!!
-              </li>
-              <li className="my-3">
-                Sometimes users are logged in yet they don't have access to the
-                application. - FIXED!!!
-              </li>
-              <li className="mb-3">
-                The app becomes a bit unusable on small devices.
-              </li>
-              <li>
-                Redirects not working properly when logging in from pages other
-                than the homepage
-              </li>
-            </ol>
 
-            <ul className="md:flex items-center justify-between mt-10 xl:max-w-3xl xl:mx-auto">
-              <li className="flex flex-col text-white mb-5 md:flex-row md:items-center md:mb-0 md:mr-5 lg:mr-20">
-                Don't have an account? &nbsp;&nbsp; <CreateAccount />
+            <div className="lg:max-w-2xl lg:mx-auto">
+              <h3 className="text-2xl text-slate-800 mt-10 mb-5">
+                Some of the known issues include:
+              </h3>
+              <ol className="list-decimal text-slate-700">
+                <li>
+                  Users needing to re-log in every time they open the
+                  application. - FIXED!!!
+                </li>
+                <li className="my-3">
+                  Sometimes users are logged in yet they don't have access to
+                  the application. - FIXED!!!
+                </li>
+                <li className="mb-3">
+                  The app becomes a bit unusable on small devices. - FIXED!!!
+                </li>
+                <li>
+                  Redirects not working properly when logging in from pages
+                  other than the homepage
+                </li>
+              </ol>
+            </div>
+
+            <ul className="md:flex items-center justify-center mt-10 xl:max-w-3xl xl:mx-auto">
+              <li className="flex flex-col text-slate-700 mb-5 md:flex-row md:items-center md:mb-0 md:mr-5">
+                <CreateAccount />
               </li>
-              <li className="flex flex-col text-white md:flex-row md:items-center">
-                Already have an account? &nbsp;&nbsp; <LogIn />
+              <li className="flex flex-col text-slate-700 md:flex-row md:items-center">
+                <LogIn />
               </li>
             </ul>
           </div>
         </div>
       </section>
+      <div className="slant-left"></div>
       <Footer />
     </>
   )
