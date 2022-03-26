56e5fcb098030ee7aa9ee416c4e5a17ba5b28ddc
Thomas Sankara <thomassankara74@gmail.com>
2022-03-26 10:38:54 +0300

    update homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,39 +3,64 @@ import hero from "../images/invoicerv2.png"
 
 export default function Home() {
   return (
-    <section className="home">
-      <section className="px-5 2xl:px-0 xl:max-w-7xl xl:mx-auto py-10">
-        <div className="overlay grid grid-cols-1 gap-5 lg:grid-cols-2 lg:items-center xl:px-0">
-          <article>
-            <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center lg:text-left mb-3 text-white">
-              Create invoices easily
-            </h1>
-            <p className="text-center lg:text-left text-xl mt-5 mb-10 text-white">
-              Easily create, download and print invoices for your clients. No
-              more unsurety, just Invoicer It!
-            </p>
+    <>
+      <section className="max-width">
+        <section className="py-10 lg:py-32">
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:place-items-center md:gap-10 lg:gap-20">
+            <article>
+              <h1 className="text-slate-700 mb-5 text-4xl md:text-5xl font-bold text-center md:text-left">
+                Create invoices with Invoicer
+              </h1>
+              <article className="md:hidden">
+                <img
+                  src={hero}
+                  alt="Create Invoices Easily"
+                  title="Create Invoices Easily"
+                  className="block mx-auto rounded"
+                />
+              </article>
+              <p className="text-slate-700 mt-5 md:mt-0 mb-5 text-center md:text-left">
+                Easily create, download and print invoices for your clients. No
+                more unsurety, just Invoicer It!
+              </p>
 
-            <ul className="flex flex-wrap items-center justify-center lg:items-start lg:justify-start mt-5">
-              <li className="mr-5">
-                <CreateAccount />
-              </li>
-              <li>
-                <LogIn />
-              </li>
-              <li></li>
-            </ul>
-          </article>
+              <ul className="flex flex-wrap items-center justify-center md:justify-start">
+                <li className="mr-3">
+                  <CreateAccount />
+                </li>
+                <li>
+                  <LogIn />
+                </li>
+              </ul>
 
-          <article>
-            <img
-              src={hero}
-              alt="Create Invoices Easily"
-              title="Create Invoices Easily"
-              className="shadow rounded"
-            />
-          </article>
-        </div>
+              <div>
+                <p className="text-slate-700 mt-5 text-center md:text-left">
+                  Do you like Invoicer? Check out my other app called{" "}
+                  <a
+                    href="https://diarry.netlify.app"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="underline text-slate-700"
+                  >
+                    Diarry
+                  </a>
+                </p>
+              </div>
+            </article>
+
+            <article className="hidden md:block">
+              <img
+                src={hero}
+                alt="Create Invoices Easily"
+                title="Create Invoices Easily"
+                className="rounded"
+              />
+            </article>
+          </div>
+        </section>
       </section>
-    </section>
+
+      <div className="slant"></div>
+    </>
   )
 }
