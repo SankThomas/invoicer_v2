8cb3fb1ccf85f030658ffaddd54d9e9765a7d4b9
Thomas Sankara <thomassankara74@gmail.com>
2022-05-02 09:39:09 +0300

    add heading

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,6 +2,7 @@ export default function Notes({ notes }) {
   return (
     <>
       <section className="mt-10 mb-5">
+        <h3>Additional notes</h3>
         <p className="lg:w-1/2 text-justify">{notes}</p>
       </section>
     </>
