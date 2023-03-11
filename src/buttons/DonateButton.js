cb740714d3cd9c39873b6d1d505db767ddbb1963
Thomas Sankara <thomassankara74@gmail.com>
2023-03-11 07:22:21 +0300

    change button to flexbox to limit width

--- src/buttons/DonateButton.js
+++ src/buttons/DonateButton.js
@@ -12,14 +12,16 @@ export default function DonateButton() {
           Support Me On Patreon
         </button>
       </a> */}
-      <a
-        href="https://www.paypal.com/donate/?hosted_button_id=U6J5KFC2KH8XG"
-        target="_blank"
-        rel="noreferrer"
-        className="bg-red-500 py-2 px-6 rounded text-base hover:bg-transparent border-2 border-red-500 text-white transition-all duration-500 hover:opacity-75 hover:bg-red-500 hover:border-red-500 shadow-lg flex items-center"
-      >
-        Donate to Creator
-      </a>
+      <div className="flex">
+        <a
+          href="https://www.paypal.com/donate/?hosted_button_id=U6J5KFC2KH8XG"
+          target="_blank"
+          rel="noreferrer"
+          className="bg-red-500 py-2 px-6 rounded text-base hover:bg-transparent border-2 border-red-500 text-white transition-all duration-500 hover:text-red-500 hover:border-red-500 shadow-lg flex items-center"
+        >
+          Donate to Creator
+        </a>
+      </div>
     </>
   );
 }
