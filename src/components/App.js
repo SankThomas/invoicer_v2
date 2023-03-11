937c2b09ab94652133c2a78a8dacf5c14d8b4139
Thomas Sankara <thomassankara74@gmail.com>
2023-03-11 07:02:24 +0300

    add invoices state value

--- src/components/App.js
+++ src/components/App.js
@@ -31,6 +31,7 @@ function App() {
   const [list, setList] = useState([])
   const [total, setTotal] = useState(0)
   const [width] = useState(641)
+  const [invoices, setInvoices] = useState([])
 
   const componentRef = useRef()
 
