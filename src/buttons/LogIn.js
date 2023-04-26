796fe72107388b7d6d2ced7c651b3914a12f5349
Thomas Sankara <thomassankara74@gmail.com>
2023-04-26 22:40:00 +0300

    update button styles

--- src/buttons/LogIn.js
+++ src/buttons/LogIn.js
@@ -1,17 +1,17 @@
-import { useContext } from "react"
-import AuthContext from "../context/auth"
+import { useContext } from "react";
+import AuthContext from "../context/auth";
 
 export default function LogIn() {
-  const { login } = useContext(AuthContext)
+  const { login } = useContext(AuthContext);
 
   return (
     <>
       <button
         onClick={login}
-        className="bg-white py-2 px-6 rounded text-base hover:bg-transparent border-2 border-white text-gray-800 transition-all duration-500 hover:opacity-75 shadow-lg hover:text-white"
+        className="bg-white py-2 px-6 rounded text-base hover:bg-gray-800 text-gray-800 transition-all duration-150 hover:text-white hover:ring-4 hover:ring-gray-400"
       >
         Log In
       </button>
     </>
-  )
+  );
 }
