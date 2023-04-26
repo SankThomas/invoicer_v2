eabb517fb894afb49e0bc7504416ef624d77565c
Thomas Sankara <thomassankara74@gmail.com>
2023-04-26 22:39:34 +0300

    update button styles

--- src/buttons/CreateAccount.js
+++ src/buttons/CreateAccount.js
@@ -8,7 +8,7 @@ export default function CreateAccount() {
     <>
       <button
         onClick={login}
-        className="bg-blue-500 py-2 px-6 rounded text-base hover:bg-transparent border-2 border-blue-500 text-white transition-all duration-500 hover:opacity-75 hover:bg-blue-800 hover:border-blue-800 shadow-lg"
+        className="bg-blue-500 py-2 px-6 rounded text-base hover:bg-transparent text-white transition-all duration-150 hover:bg-blue-600 shadow-lg hover:ring-4 hover:ring-blue-400"
       >
         Log In or Create Account
       </button>
