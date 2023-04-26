b74ef2213c47794b2320b0bba62a0c65075531b1
Thomas Sankara <thomassankara74@gmail.com>
2023-04-26 22:40:11 +0300

    update button styles

--- src/buttons/LogOut.js
+++ src/buttons/LogOut.js
@@ -1,17 +1,24 @@
-import { useContext } from "react"
-import AuthContext from "../context/auth"
+import { useContext, useState } from "react";
+import AuthContext from "../context/auth";
+import Logout from "../components/Logout";
 
 export default function LogOut() {
-  const { logout } = useContext(AuthContext)
+  const { logout } = useContext(AuthContext);
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   return (
     <>
       <button
-        onClick={logout}
-        className="bg-white py-2 px-6 rounded text-base hover:bg-transparent border-2 border-white text-gray-800 transition-all duration-500 hover:opacity-75 shadow-lg hover:border-gray-800"
+        onClick={() => setShowLogoutModal(true)}
+        className="bg-white py-2 px-6 rounded text-base hover:bg-gray-800 text-gray-800 transition-all duration-150 hover:text-white hover:ring-4 hover:ring-gray-400"
       >
         Log Out
       </button>
+
+      {/* Logout component modal */}
+      {showLogoutModal && (
+        <Logout logout={logout} setShowLogoutModal={setShowLogoutModal} />
+      )}
     </>
-  )
+  );
 }
