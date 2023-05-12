fbe316b1fb9b4ebb3e83ab145a018c459a95de87
Thomas Sankara <thomassankara74@gmail.com>
2023-05-12 13:08:32 +0300

    fix logout modal

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,9 +3,11 @@ import { BsEmojiSmile } from "react-icons/bs";
 import { CgSmileSad } from "react-icons/cg";
 import { GrClose } from "react-icons/gr";
 import { State } from "../context/stateContext";
+import AuthContext from "../context/auth";
 
 export default function Logout() {
-  const { setShowLogoutModal, logout } = useContext(State);
+  const { setShowLogoutModal } = useContext(State);
+  const { logout } = useContext(AuthContext);
 
   return (
     <div className="transition-all duration-200 bg-black bg-opacity-75 fixed left-0 right-0 bottom-0 top-0 z-20">
