102af47b27caad522fad82e1ef129f5cbebb5f8f
Thomas Sankara <thomassankara74@gmail.com>
2023-05-12 12:50:23 +0300

    add context + remove props on components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
-export default function Footer({
-  name,
-  email,
-  website,
-  phone,
-  bankAccount,
-  bankName,
-}) {
+import { useContext } from "react";
+import { State } from "../context/stateContext";
+
+export default function Footer() {
+  const { name, email, website, phone, bankAccount, bankName } =
+    useContext(State);
+
   return (
     <>
       <footer className="footer border-t-2 border-gray-300 pt-5">
@@ -36,6 +35,18 @@ export default function Footer({
           </li>
         </ul>
       </footer>
+
+      <p className="text-center px-5 mt-8 text-xs ">
+        Invoicer is built by{" "}
+        <a
+          href="https://tsbsankara.com"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          Thomas Sankara
+        </a>
+      </p>
     </>
-  )
+  );
 }
