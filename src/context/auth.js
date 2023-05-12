451613dbe8d0552f4738f1c4975a3b6166a4d795
Thomas Sankara <thomassankara74@gmail.com>
2023-05-12 12:51:36 +0300

    add semicolons

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,45 +1,47 @@
-import { createContext, useState, useEffect } from "react"
-import netlifyIdentity from "netlify-identity-widget"
+import { createContext, useState, useEffect } from "react";
+import netlifyIdentity from "netlify-identity-widget";
 
 const AuthContext = createContext({
   user: null,
   login: () => {},
   logout: () => {},
   isAuthenticated: false,
-})
+});
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     netlifyIdentity.on("login", (user) => {
-      setUser(user)
-      netlifyIdentity.close()
-    })
+      setUser(user);
+      netlifyIdentity.close();
+    });
 
     netlifyIdentity.on("logout", () => {
-      setUser(null)
-    })
+      setUser(null);
+    });
 
     netlifyIdentity.on("init", (user) => {
-      setUser(user)
-    })
+      setUser(user);
+    });
 
-    netlifyIdentity.init()
+    netlifyIdentity.init();
 
     return () => {
-      netlifyIdentity.off("login")
-      netlifyIdentity.off("logout")
-    }
-  }, [])
+      netlifyIdentity.off("login");
+      netlifyIdentity.off("logout");
+    };
+  }, []);
 
-  const login = () => netlifyIdentity.open()
+  const login = () => netlifyIdentity.open();
 
-  const logout = () => netlifyIdentity.logout()
+  const logout = () => netlifyIdentity.logout();
 
-  const context = { user, login, logout }
+  const context = { user, login, logout };
 
-  return <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
-}
+  return (
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
+};
 
-export default AuthContext
+export default AuthContext;
