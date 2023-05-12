64d6bde8b9ed3a7f2076ac2a2da14be6c9e38d68
Thomas Sankara <thomassankara74@gmail.com>
2023-05-12 12:50:30 +0300

    add context + remove props on components

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BsEmojiSmile } from "react-icons/bs";
 import { CgSmileSad } from "react-icons/cg";
 import { GrClose } from "react-icons/gr";
+import { State } from "../context/stateContext";
 
-export default function Logout({ setShowLogoutModal, logout }) {
+export default function Logout() {
+  const { setShowLogoutModal, logout } = useContext(State);
+
   return (
     <div className="transition-all duration-200 bg-black bg-opacity-75 fixed left-0 right-0 bottom-0 top-0 z-20">
       <div className="w-9/12 max-w-2xl bg-white p-8 rounded shadow fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
